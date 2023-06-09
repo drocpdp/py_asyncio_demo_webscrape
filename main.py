b0dce0f8bba784c5f435c4547835fe4dbe3213dc
drocpdp <davidreynon@gmail.com>
2023-06-09 15:00:55 +0200

    await sleeps for more concurrency

--- main.py
+++ main.py
@@ -16,27 +16,35 @@ websites = ['http://davidreynon.com', 'https://cnn.com']
 """
 async def request(site):
     print('request()' + site)
-    time.sleep(random.randint(1,3))
+    await asyncio.sleep(random.randint(1,3))
     print ('response from ', site)
     return 'response ' + site
 
 async def etl(line, site):
     print('etl()' + line + site)
-    time.sleep(random.randint(1,3))
+    await asyncio.sleep(random.randint(1,3))
     print ('completed etl for ', line, site)
     return 'etl ' + site
 
 async def write_to_db(data, site):
     print('write_to_db()' + data + site)
-    time.sleep(random.randint(1,3))
+    await asyncio.sleep(random.randint(1,3))
     print ('write_to_db() complete', data, site)
     return 'write_to_db' + site
 
+async def site_process(site):
+    response = await request(site)
+    etl_status = await etl(response, site)
+    db_write_status = await write_to_db('fake data', site)
+    return
+
 async def main():
-    for website in websites:
-        response = await request(website)
-        etl_status = await etl(response, website)
-        db_write_status = await write_to_db('fake data', website)
+    websites_group = await asyncio.gather(
+        *[site_process(site) for site in websites]
+    )
+
+        
+        
 
 
 main = asyncio.run(main())
